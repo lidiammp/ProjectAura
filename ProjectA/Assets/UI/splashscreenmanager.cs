71cb3a4f662fc3bed4eb236740a96e2451295675
mliziz <102434601+mliziz@users.noreply.github.com>
2025-07-25 20:01:07 -0700

    made the menu work!

--- ProjectA/Assets/UI/splashscreenmanager.cs
+++ ProjectA/Assets/UI/splashscreenmanager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class newscene : MonoBehaviour
+
+public class splashscreenmanager : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
 }
