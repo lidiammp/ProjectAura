8c22e9401f99ae02f7ef84f1ae6f225fdb95320d
Zion Koundou <114265782+ZiKoundou@users.noreply.github.com>
2025-08-01 03:50:45 -0700

    Updated first section and modified embrace to only move player horizontally for big guys

--- ProjectA/Assets/Scripts/PlayerEmbrace.cs
+++ ProjectA/Assets/Scripts/PlayerEmbrace.cs
@@ -146,7 +146,7 @@ public class PlayerEmbrace : MonoBehaviour
         while ((enemy.transform.position - transform.position).magnitude > enemy.snapThreshold)
         {
             Vector3 direction = (enemy.transform.position - transform.position).normalized;
-            playerMovement.Move(snapSpeed, direction);
+            playerMovement.Move(snapSpeed, new Vector3 (direction.x, 0, direction.z));
             playerMovement.GetComponent<MouseLook>().RotateToPoint(enemy.transform);
             yield return null;
         }
