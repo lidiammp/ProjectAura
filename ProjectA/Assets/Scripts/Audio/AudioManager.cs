ef818d1c56830ac253ca204a3629de0b56c73332
Zion Koundou <kodesone13@gmail.com>
2026-05-06 22:53:23 -0700

    Imported new sounds into project and adding audio pool

--- ProjectA/Assets/Scripts/Audio/AudioManager.cs
+++ ProjectA/Assets/Scripts/Audio/AudioManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Diagnostics;
 using System.Reflection;
-
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
@@ -13,12 +13,13 @@ public class AudioManager : MonoBehaviour
     // audio source
     [Header("------------- Audio Source ------------")]
     [SerializeField] AudioSource musicSource;
-    [SerializeField] AudioSource SFXSource;
     [Header("------------- Sounds------------")]
     
+    public AudioMixerGroup sfxGroup;
     public Sound[] musicSounds, sfxSounds;
     public static AudioManager instance;
-
+    private AudioSource[] pool;
+    public int poolSize = 10;
     // Start is called before the first frame update
     void Awake()
     {
@@ -31,6 +32,19 @@ public class AudioManager : MonoBehaviour
         }
         //if theres no instance of this orig, create a new one
         instance = this;
+
+        pool = new AudioSource[poolSize];
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject go = new GameObject("AudioSource_" + i);
+            go.transform.parent = transform;
+
+            AudioSource src = go.AddComponent<AudioSource>();
+            src.playOnAwake = false;
+            src.spatialBlend = 1f; // 3D sound
+            src.outputAudioMixerGroup = sfxGroup;
+            pool[i] = src;
+        }
     }
     void Start()
     {
@@ -53,13 +67,15 @@ public class AudioManager : MonoBehaviour
             if(musicSource.clip == s.clip) return;
             musicSource.clip = s.clip;
             musicSource.loop = true;
-            SFXSource.pitch = s.pitch;
+            musicSource.pitch = s.pitch;
             musicSource.Play();
         }
     }
 
     public void PlaySFX(string name)
     {
+        AudioSource SFXSource = GetFreeSource();
+       
         Sound s = Array.Find(sfxSounds, x => x.name==name);
         if(s == null)
         {
@@ -69,12 +85,15 @@ public class AudioManager : MonoBehaviour
         {   
             SFXSource.loop = false;
             SFXSource.pitch = s.pitch;
-            SFXSource.PlayOneShot(s.clip, s.volume);
+            SFXSource.clip = s.clip;
+            SFXSource.volume = s.volume;
+            SFXSource.Play();
         }
     }
 
     public void PlaySFXRandom(string name)
     {
+        AudioSource SFXSource = GetFreeSource();
         Sound s = Array.Find(sfxSounds, x => x.name==name);
         if(s == null)
         {
@@ -82,11 +101,11 @@ public class AudioManager : MonoBehaviour
         }
         else
         {   
-            float originalPitch = SFXSource.pitch;
             SFXSource.loop = false;
             SFXSource.pitch = UnityEngine.Random.Range(s.pitchStart, s.pitchEnd);
-            SFXSource.PlayOneShot(s.clip, s.volume);
-            SFXSource.pitch = originalPitch;
+            SFXSource.clip = s.clip;
+            SFXSource.volume = s.volume;
+            SFXSource.Play();
         }
     }
 
@@ -125,6 +144,19 @@ public class AudioManager : MonoBehaviour
             source.PlayOneShot(s.clip, s.volume);
             source.pitch = originalPitch;
         }
+    }
+
+    public AudioSource GetFreeSource()
+    {
+        foreach (var source in pool)
+        {
+            if (!source.isPlaying)
+            {
+                return source;
+            }
+        }
+
+        return pool[0]; //use the first if all are busy
     }
     void OnDestroy()
     {
